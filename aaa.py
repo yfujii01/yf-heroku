2450bdf8e459a2ed5cf54cd4e1f6a134d5e85371
yfujii01 <fancl01@gmail.com>
2018-06-04 07:53:14 +0900

    update

--- aaa.py
+++ aaa.py
@@ -1,4 +1,6 @@
 import os
+import psycopg2
+from psycopg2.extras import DictCursor
 
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -20,3 +22,21 @@ else:
 def exe():
     print('hello')
     return 'xyz'
+
+
+def get_connection():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return conn
+
+
+def get_param():
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute('SELECT * FROM t_param')
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return result
